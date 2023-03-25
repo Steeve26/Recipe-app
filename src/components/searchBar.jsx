02b44a675e8af09e3ca15852194b4bcc765b1c8c
Estifanos Wendimu <114855602+Steeve26@users.noreply.github.com>
2023-03-25 14:56:40 +0300

    minor functionality changes

--- src/components/searchBar.jsx
+++ src/components/searchBar.jsx
@@ -1,14 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react'
 import {MdCancel} from 'react-icons/md'
 import './searchBar.css'
 
 export default function searchBar() {
+  const [searchInput, setSearchInput] = useState('');
+
+  function clear() {
+    setSearchInput('')
+  }
+
   return (
     <div className='searchBar-wrapper'>
-      <input type="text" placeholder='Search'/>
-      <button className="clear-btn">
+      <input onChange={(e) => setSearchInput(e.target.value)} value={searchInput} type="text" placeholder='Search'/>
+      {searchInput && <button onClick={() => clear()} className="clear-btn">
         <MdCancel color='white' size={25}/>                
-      </button>
+      </button>}
     </div>
   )
 }
