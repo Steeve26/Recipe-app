066b906d6cbdbebb02dccce13d3856dafeed3312
Estifanos Wendimu <114855602+Steeve26@users.noreply.github.com>
2023-03-25 14:53:46 +0300

    routing added

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,18 @@
 import { useState } from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './App.css'
 import Home from './pages/home'
-
 
 function App() {
 
   return (
     <div className="App">
-      <Home/>
+      <h1>🍴 delicioso</h1>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Home/>}></Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   )
 }
