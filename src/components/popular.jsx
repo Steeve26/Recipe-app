8671f741c871b194225d60e40ffd38f1a9053e49
Steve-bro <114855602+Steve-bro@users.noreply.github.com>
2023-03-30 11:58:36 +0300

    api bug fixes

--- src/components/popular.jsx
+++ src/components/popular.jsx
@@ -18,7 +18,7 @@ export default function popular() {
       {
         const api = await fetch (`https://api.spoonacular.com/recipes/random?apiKey=${import.meta.env.VITE_APP_API_KEY}&number=9`)
         const data = await api.json();
-        localStorage.setItem('popular', JSON.stringify(data.recipes))
+        localStorage.setItem('popular', json.stringify(data.recipes))
         setPopular(data.recipes)
       }
 
