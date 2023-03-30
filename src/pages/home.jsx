0557d80b16ba1fbc5f3429764c732e3d4d0952d2
Steve-bro <114855602+Steve-bro@users.noreply.github.com>
2023-03-30 11:44:41 +0300

    api changes and unsused code removal

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,8 +1,4 @@
 import React from 'react'
-import Popular from '../components/popular'
-import Veggies from '../components/veggies'
-import Categories from '../components/categories'
-import SearchBar from '../components/searchBar'
 import { NavLink, Route, Routes } from 'react-router-dom'
 import '../App.css'
 import Slides from './slides'
